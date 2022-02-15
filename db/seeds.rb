8bfd2fa6a28c4e9045d856b0e955ecf33a6d2f0c
camille breillot <camille.breillot@gmail.com>
2022-02-15 18:35:28 +0100

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating users..."
+
+marc = User.create(email: "marc@gmail.com", password: "password", username: "Marc")
+camille = User.create(email: "camille@gmail.com", password: "password", username: "Camille")
+puts "Users created..."
+
+puts "Creating collects..."
+collect1 = Collection.create(name: "Collecte de denrées alimentaire 1", asso: "Restos du Coeur", date: "04/12/2021", user_id: marc.id)
+collect2 = Collection.create(name: "Collecte de denrées alimentaire 2", asso: "Banque Alimentaire", date: "15/01/2022", user_id: marc.id)
+collect3 = Collection.create(name: "Collecte de denrées alimentaire 3", asso: "Secours Populaire", date: "10/02/2022", user_id: marc.id)
+puts "Collects created..."
+
+puts "Creating indicators..."
+indicator1 = Indicator.create(name: "Denrées collectées", value: 250, collection_id: collect1.id)
+indicator2 = Indicator.create(name: "Participants", value: 100, collection_id: collect1.id)
+indicator3 = Indicator.create(name: "Durée", value: 2, collection_id: collect1.id)
+indicator4 = Indicator.create(name: "Denrées collectées", value: 500, collection_id: collect2.id)
+indicator5 = Indicator.create(name: "Participants", value: 200, collection_id: collect2.id)
+indicator6 = Indicator.create(name: "Durée", value: 4, collection_id: collect2.id)
+indicator7 = Indicator.create(name: "Denrées collectées", value: 600, collection_id: collect3.id)
+indicator8 = Indicator.create(name: "Participants", value: 225, collection_id: collect3.id)
+indicator9 = Indicator.create(name: "Durée", value: 5, collection_id: collect3.id)
+puts "Indicators created..."
