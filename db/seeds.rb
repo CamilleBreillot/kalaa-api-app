49ad6da89d7169516c12a9f4a965a4c984a28b7f
camille breillot <camille.breillot@gmail.com>
2022-02-15 19:16:53 +0100

    cors added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,18 @@
-puts "Creating users..."
+puts "Cleaning database"
+User.destroy_all
+Collection.destroy_all
+Indicator.destroy_all
 
+puts "Creating users..."
 marc = User.create(email: "marc@gmail.com", password: "password", username: "Marc")
 camille = User.create(email: "camille@gmail.com", password: "password", username: "Camille")
-puts "Users created..."
+puts "Users created!"
 
 puts "Creating collects..."
 collect1 = Collection.create(name: "Collecte de denrées alimentaire 1", asso: "Restos du Coeur", date: "04/12/2021", user_id: marc.id)
 collect2 = Collection.create(name: "Collecte de denrées alimentaire 2", asso: "Banque Alimentaire", date: "15/01/2022", user_id: marc.id)
 collect3 = Collection.create(name: "Collecte de denrées alimentaire 3", asso: "Secours Populaire", date: "10/02/2022", user_id: marc.id)
-puts "Collects created..."
+puts "Collects created!"
 
 puts "Creating indicators..."
 indicator1 = Indicator.create(name: "Denrées collectées", value: 250, collection_id: collect1.id)
@@ -20,4 +24,6 @@ indicator6 = Indicator.create(name: "Durée", value: 4, collection_id: collect2.
 indicator7 = Indicator.create(name: "Denrées collectées", value: 600, collection_id: collect3.id)
 indicator8 = Indicator.create(name: "Participants", value: 225, collection_id: collect3.id)
 indicator9 = Indicator.create(name: "Durée", value: 5, collection_id: collect3.id)
-puts "Indicators created..."
+puts "Indicators created!"
+
+puts "Success, seed created!"
